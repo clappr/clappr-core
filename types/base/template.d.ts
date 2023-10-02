d689f618a7dd3833907c780b2117114a7a89a0c7
Francisco Vieira <francisco.vieira_thoughtworks@prestador.globo>
2023-10-02 00:28:35 -0300

    feat(typescript): add declaration for base/template

--- types/base/template.d.ts
+++ types/base/template.d.ts
@@ -1,10 +1,30 @@
 export default tmpl;
-declare function tmpl(text: any, data: any): any;
+export type SettingsTmplType = {
+    evaluate: RegExp;
+    interpolate: RegExp;
+    escape: RegExp;
+};
+/**
+ * JavaScript micro-templating, similar to John Resig's implementation.
+ * Underscore templating handles arbitrary delimiters, preserves whitespace,
+ * and correctly escapes quotes within interpolated code.
+ *
+ * @param {string} text
+ * @param {*=} data
+ * @returns
+ */
+declare function tmpl(text: string, data?: any | undefined): any;
 declare namespace tmpl {
     export { settings };
 }
-declare namespace settings {
-    let evaluate: RegExp;
-    let interpolate: RegExp;
-    let escape: RegExp;
-}
+/**
+ * @typedef SettingsTmplType
+ * @type {object}
+ * @property {RegExp} evaluate
+ * @property {RegExp} interpolate
+ * @property {RegExp} escape
+ */
+/**
+ * @type {SettingsTmplType}
+ */
+declare var settings: SettingsTmplType;
